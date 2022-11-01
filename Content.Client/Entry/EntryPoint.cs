85f71d4cfbb559ce6945591b3f086621ca5a485b
Igor Spichkin <igorsa.ux@outlook.com>
2022-11-01 20:44:42 +0300

    fix: nullspace existance

--- Content.Client/Entry/EntryPoint.cs
+++ Content.Client/Entry/EntryPoint.cs
@@ -141,7 +141,6 @@ public sealed class EntryPoint : GameClient
         _ghostKick.Initialize();
         _extendedDisconnectInformation.Initialize();
         _playTimeTracking.Initialize();
-        _baseClient.PlayerJoinedServer += (_, _) => { _mapManager.CreateNewMapEntity(MapId.Nullspace); };
 
         //AUTOSCALING default Setup!
         _configManager.SetCVar("interface.resolutionAutoScaleUpperCutoffX", 1080);
