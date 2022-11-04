350e871af87ce85c1a69b28957405f6194f42928
Igor Spichkin <igorsa.ux@outlook.com>
2022-11-04 16:55:46 +0300

    removes beforeserialization hook (#12319)

--- Content.Shared/Doors/Components/DoorComponent.cs
+++ Content.Shared/Doors/Components/DoorComponent.cs
@@ -1,8 +1,8 @@
 using Content.Shared.Damage;
 using Content.Shared.Tools;
+using JetBrains.Annotations;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
